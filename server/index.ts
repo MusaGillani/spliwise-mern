6719ee1b8f38bf5ea062c037eb4c12a30d7cfaa4
SyedMusaGillani <musa.gillani@devsinc.com>
2022-11-07 21:30:21 +0500

    linter configuration

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express'
 
-const app: Express = express();
-const port = 3000;
+const app: Express = express()
+const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
